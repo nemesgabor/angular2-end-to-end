aa936b98f7ebc373107f8d0446218ae769d89806
nemesgabor <nemesgabor@outlook.com>
2017-05-01 13:24:34 +0200

    Test

---
+++ src/app/admin/adminShared/blog-admin.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Blog } from '../adminShared/blog';
+
+@Injectable()
+
+export class BlogAdminService {
+    
+}
